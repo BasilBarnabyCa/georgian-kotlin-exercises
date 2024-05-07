8d30a201b1022dbd795d81abe78ddd05affc43f2
Basil Barnaby <basil.barnaby@devmassive.com>
2024-05-07 15:18:31 -0400

    Programmatically added a greeting to helloWorldTextView

--- ICE01/app/src/main/java/ca/georgiancollege/ice01/MainActivity.kt
+++ ICE01/app/src/main/java/ca/georgiancollege/ice01/MainActivity.kt
@@ -20,6 +20,7 @@ class MainActivity : AppCompatActivity()
         }
 
         val helloWorldTextView = findViewById<TextView>(R.id.helloWorldTextView)
-        helloWorldTextView.text = getString(R.string.good_bye_string)
+        //helloWorldTextView.text = getString(R.string.good_bye_string)
+        helloWorldTextView.text = getString(R.string.greeting_string)
     }
 }
