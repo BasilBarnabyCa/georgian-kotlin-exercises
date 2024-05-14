1dc2b4f8b7ada975939fccf5735b1620bf96dadd
Basil Barnaby <basil.barnaby@devmassive.com>
2024-05-14 14:03:58 -0400

    Converted project to use view binding

--- ICE02/app/src/main/java/ca/georgiancollege/ice02/MainActivity.kt
+++ ICE02/app/src/main/java/ca/georgiancollege/ice02/MainActivity.kt
@@ -6,20 +6,27 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import ca.georgiancollege.ice02.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity()
 {
+    // Initializes binding with the shape of ActivityMainBinding
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        //creates reference to the ActivityMainBinding Class Object
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
 
-        val helloWorldTextView = findViewById<TextView>(R.id.helloWorldTextView)
-        helloWorldTextView.text = getString(R.string.greeting_string)
+        val helloWorldString = binding.helloWorldTextView
+        helloWorldString.text = getString(R.string.greeting_string)
     }
 }
