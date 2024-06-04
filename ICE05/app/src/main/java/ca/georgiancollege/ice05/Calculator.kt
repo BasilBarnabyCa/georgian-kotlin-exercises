a8863ac46ffb4f5d6a89db54c5584af96390af2b
Basil Barnaby <basilbarnaby.ca@gmail.com>
2024-06-04 14:44:39 -0400

    Added when case for decimal and delete

--- ICE05/app/src/main/java/ca/georgiancollege/ice05/Calculator.kt
+++ ICE05/app/src/main/java/ca/georgiancollege/ice05/Calculator.kt
@@ -14,7 +14,7 @@ class Calculator (private val context:Context, dataBinding: ActivityMainBinding)
     }
 
     private fun createButtons() {
-        val digitButtons = arrayOf(
+        val operandButtons = arrayOf(
             binding.oneButton,
             binding.twoButton,
             binding.threeButton,
@@ -39,7 +39,7 @@ class Calculator (private val context:Context, dataBinding: ActivityMainBinding)
             binding.percentButton
         )
 
-        digitButtons.forEach { it.setOnClickListener { digitPressHandler(it.tag as String) } }
+        operandButtons.forEach { it.setOnClickListener { operandPressHandler(it.tag as String) } }
 //        operationButtons.forEach { it.setOnClickListener { operationPressHandler(it.tag as String) } }
 
         binding.clearButton.setOnClickListener {
@@ -50,9 +50,9 @@ class Calculator (private val context:Context, dataBinding: ActivityMainBinding)
             deleteCharacter()
         }
 
-        binding.decimalButton.setOnClickListener {
-            addDecimal()
-        }
+//        binding.decimalButton.setOnClickListener {
+//            addDecimal()
+//        }
 
         binding.plusMinusButton.setOnClickListener {
             togglePlusMinus()
@@ -60,28 +60,35 @@ class Calculator (private val context:Context, dataBinding: ActivityMainBinding)
 
     }
 
-    private fun digitPressHandler(tag: String) = when(tag)
-    {
-        "." -> {}
-        "delete" -> {}
-        "plus_minus" -> {}
-        else -> {
-            if(binding.resultTextView.text == "0")
-            {
-                binding.resultTextView.text = tag
-            } else {
-                result += tag
-                binding.resultTextView.text = result
+    private fun operandPressHandler(tag: String) {
+        when(tag)
+        {
+            "." -> {
+                if(!binding.resultTextView.text.contains("."))
+                {
+                    result += if(result.isEmpty()) "0." else "."
+                    binding.resultTextView.text = result
+                }
+            }
+            "delete" -> {
+                result = result.dropLast(1)
+                binding.resultTextView.text = if(result.isEmpty()) "0" else result
+            }
+            "plus_minus" -> {}
+            else -> {
+
+                if(binding.resultTextView.text == "0")
+                {
+                    result = tag
+                    binding.resultTextView.text = result
+                }
+                else
+                {
+                    result += tag
+                    binding.resultTextView.text = result
+                }
             }
         }
-
-
-//        val currentValue = binding.resultTextView.text
-//        if(binding.resultTextView.text == "0") {
-//            binding.resultTextView.text = tag
-//        } else {
-//            binding.resultTextView.text = String.format("%s%s", currentValue, tag)
-//        }
     }
 
     private fun operationPressHandler(tag: String)
