43119340f7533560b3bf9c1535f301ec027641a4
Basil Barnaby <basilbarnaby.ca@gmail.com>
2024-06-11 13:02:23 -0400

    Added ICE06 project folder

---
+++ ICE06/app/src/main/java/ca/georgiancollege/ice06/MainActivity.kt
@@ -0,0 +1,31 @@
+package ca.georgiancollege.ice06
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import ca.georgiancollege.ice06.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity()
+{
+    // Initializes binding with the shape of ActivityMainBinding
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        //Creates a reference to the ActivityMainBinding Class Object
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        enableEdgeToEdge()
+        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val calculator = Calculator(binding)
+    }
+}
