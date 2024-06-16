74fc428544a0ff63eb7e122ba75847eb2a2f7168
Basil Barnaby <basilbarnaby.ca@gmail.com>
2024-06-16 10:28:59 -0400

    Added functions for subtract, multiply, dividen and show answer

--- ICE05/app/src/main/java/ca/georgiancollege/ice05/Calculator.kt
+++ ICE05/app/src/main/java/ca/georgiancollege/ice05/Calculator.kt
@@ -1,14 +1,19 @@
 package ca.georgiancollege.ice05
 
+import android.util.Log
 import ca.georgiancollege.ice05.databinding.ActivityMainBinding
 
-class Calculator (dataBinding: ActivityMainBinding){
+class Calculator(dataBinding: ActivityMainBinding) {
 
     private var binding: ActivityMainBinding = dataBinding
     private var result: String
+    private var currentOperand: String
+    private var currentOperator: String
 
     init {
         result = "0"
+        currentOperand = ""
+        currentOperator = ""
         createButtons()
     }
 
@@ -30,7 +35,16 @@ class Calculator (dataBinding: ActivityMainBinding){
             binding.plusMinusButton
         )
 
+        val operatorButtons = arrayOf(
+            binding.plusButton,
+            binding.minusButton,
+            binding.multiplicationButton,
+            binding.divideButton,
+            binding.equalsButton
+        )
+
         operandButtons.forEach { it.setOnClickListener { operandPressHandler(it.tag as String) } }
+        operatorButtons.forEach { it.setOnClickListener { operatorPressHandler(it.tag as String) } }
 
         binding.clearButton.setOnClickListener {
             clearScreen()
@@ -38,45 +52,40 @@ class Calculator (dataBinding: ActivityMainBinding){
     }
 
     private fun operandPressHandler(tag: String) {
-        when(tag)
-        {
+        when (tag) {
             "." -> {
-                if(!binding.resultTextView.text.contains("."))
-                {
-                    result += if(result.isEmpty()) "0." else "."
+                if (!binding.resultTextView.text.contains(".")) {
+                    result += if (result.isEmpty()) "0." else "."
                     binding.resultTextView.text = result
                 }
             }
+
             "delete" -> {
                 result = result.dropLast(1)
-                binding.resultTextView.text = if(result.isEmpty() || result == "-") "0" else result
-                result = if(result.isEmpty()) "0" else result
+                binding.resultTextView.text = if (result.isEmpty() || result == "-") "0" else result
+                result = if (result.isEmpty()) "0" else result
             }
+
             "plus_minus" -> {
-                if(result.startsWith("-"))
-                {
+                if (result.startsWith("-")) {
                     result = result.substring(1)
-                }
-                else
-                {
-                    if(result.isNotEmpty() && result != "0")
-                    {
+                } else {
+                    if (result.isNotEmpty() && result != "0") {
                         result = "-".plus(result)
                     }
                 }
                 binding.resultTextView.text = result
             }
-             "clear" -> {
-                 clearScreen()
-             }
+
+            "clear" -> {
+                clearScreen()
+            }
+
             else -> {
-                if(binding.resultTextView.text == "0")
-                {
+                if (binding.resultTextView.text == "0") {
                     result = tag
                     binding.resultTextView.text = result
-                }
-                else
-                {
+                } else {
                     result += tag
                     binding.resultTextView.text = result
                 }
@@ -84,9 +93,83 @@ class Calculator (dataBinding: ActivityMainBinding){
         }
     }
 
-    private fun clearScreen()
-    {
+    private fun operatorPressHandler(tag: String) {
+        if (currentOperator.isNotEmpty()) {
+            when (currentOperator) {
+                "addition" -> {
+                    add()
+                }
+                "subtraction" -> {
+                    subtract()
+                }
+                "multiplication" -> {
+                   multiply()
+                }
+                "division" -> {
+                    divide()
+                }
+                else -> {
+                    showAnswer()
+                }
+
+            }
+        } else {
+            currentOperand = binding.resultTextView.text.toString()
+            result = "0"
+            binding.resultTextView.text = "0"
+        }
+        currentOperator = tag;
+    }
+
+    private fun clearScreen() {
         result = "0"
         binding.resultTextView.text = "0"
+        currentOperator = ""
+        currentOperand = ""
+    }
+
+    private fun add() {
+        if (currentOperand.contains(".") || result.contains(".")) {
+            result = (currentOperand.toDouble() + result.toDouble()).toString()
+        } else {
+            result = (currentOperand.toInt() + result.toInt()).toString()
+        }
+        currentOperand = result
+    }
+
+    private fun subtract() {
+        if (currentOperand.contains(".") || result.contains(".")) {
+            result = (currentOperand.toDouble() - result.toDouble()).toString()
+        } else {
+            result = (currentOperand.toInt() - result.toInt()).toString()
+        }
+        currentOperand = result
+    }
+
+    private fun multiply()
+    {
+        if (currentOperand.contains(".") || result.contains(".")) {
+            result = (currentOperand.toDouble() * result.toDouble()).toString()
+        } else {
+            result = (currentOperand.toInt() * result.toInt()).toString()
+        }
+        currentOperand = result
+    }
+
+    private fun divide()
+    {
+        if (currentOperand.contains(".") || result.contains(".")) {
+            result = (currentOperand.toDouble() / result.toDouble()).toString()
+        } else {
+            result = (currentOperand.toInt() / result.toInt()).toString()
+        }
+        currentOperand = result
+    }
+
+    private fun showAnswer()
+    {
+        binding.resultTextView.text = currentOperand
+        currentOperand = ""
+        currentOperator = ""
     }
 }
