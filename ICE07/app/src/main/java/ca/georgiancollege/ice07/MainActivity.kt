6a26c46d8874e2d13876b40456d4537079c43bc8
Basil Barnaby <basilbarnaby.ca@gmail.com>
2024-07-02 13:43:58 -0400

    Added Fisrt Adapter Class

--- ICE07/app/src/main/java/ca/georgiancollege/ice07/MainActivity.kt
+++ ICE07/app/src/main/java/ca/georgiancollege/ice07/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
 import ca.georgiancollege.ice07.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -14,6 +15,22 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
-        enableEdgeToEdge()
+        setContentView(binding.root)
+
+        // create an array of TV shows (mock data)
+        val favouriteTVShows = arrayOf(
+            TVShow("House of the Dragon", "HBO"),
+            TVShow("Lord of the Rings", "Prime Video"),
+            TVShow("Andor", "Disney"),
+            TVShow("Severance", "AppleTv"),
+            TVShow("Star Trek: Strange New Worlds", "Paramount+")
+        )
+
+        // create an instance of the FirstAdapter and pass in the array of TV shows
+        val firstAdapter = FirstAdapter(favouriteTVShows)
+        binding.firstRecyclerView.apply {
+            layoutManager = LinearLayoutManager(context)
+            adapter = firstAdapter
+        }
     }
 }
