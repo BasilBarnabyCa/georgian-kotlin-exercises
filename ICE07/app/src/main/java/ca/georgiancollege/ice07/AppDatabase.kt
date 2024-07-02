cc04c2ed93ca6e6174d454109666e9f43c8ec3ee
Basil Barnaby <basilbarnaby.ca@gmail.com>
2024-07-02 14:50:39 -0400

    Added DataManager singleton

--- ICE07/app/src/main/java/ca/georgiancollege/ice07/AppDatabase.kt
+++ ICE07/app/src/main/java/ca/georgiancollege/ice07/AppDatabase.kt
@@ -12,28 +12,29 @@ abstract class AppDatabase: RoomDatabase()
 
     companion object
     {
+
         // Volatile annotation marks the variable as visible to all threads
         @Volatile
         private  var m_instance: AppDatabase? = null
-    }
 
-    // Multiple threads can access the database at the same time
-    // ensuuring that only one thread can access the database at a time
-    fun getDatabase(context: Context): AppDatabase
-    {
-        return m_instance ?: synchronized(this) {
-            // Create an instance of the database using Room.DatabaseBuilder
-            val instance = Room.databaseBuilder(
-                context.applicationContext,
-                AppDatabase::class.java,
-                "media_database"
-            ).build()
+        // Multiple threads can access the database at the same time
+        // ensuring that only one thread can access the database at a time
+        fun getDatabase(context: Context): AppDatabase
+        {
+            return m_instance ?: synchronized(this) {
+                // Create an instance of the database using Room.DatabaseBuilder
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    AppDatabase::class.java,
+                    "media_database"
+                ).build()
 
-            // Assign the newly created database instance to the m_instance variable
-            m_instance = instance
+                // Assign the newly created database instance to the m_instance variable
+                m_instance = instance
 
-            // Return the instance
-            instance
+                // Return the instance
+                instance
+            }
         }
     }
 }
