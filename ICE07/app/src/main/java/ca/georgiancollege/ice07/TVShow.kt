9177a92b4870de0f1f9fac5c3e6ae72958b825ac
Basil Barnaby <basilbarnaby.ca@gmail.com>
2024-07-02 14:24:46 -0400

    Updated Tv show data class to use ROOM

--- ICE07/app/src/main/java/ca/georgiancollege/ice07/TVShow.kt
+++ ICE07/app/src/main/java/ca/georgiancollege/ice07/TVShow.kt
@@ -1,7 +1,23 @@
 package ca.georgiancollege.ice07
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+// Entity annotation marks this class as a table in the room database
+@Entity(tableName = "tv_shows")
+
 // Model Class
 data class TVShow(
+    // Marks this field as a primary key and autogenerate makes the field autogenerate
+    @PrimaryKey(autoGenerate = true)
+    val id: Int = 0,
+
+    // Title column
     val title: String,
-    val subTitle: String
+
+    // Genre column
+    val genre: String,
+
+    // Rating column
+    val rating: Double
 )
