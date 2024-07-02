27a3f6247502973c18b775ddbab2335377ddd256
Basil Barnaby <basilbarnaby.ca@gmail.com>
2024-07-02 15:04:59 -0400

    Updated text row item layout

---
+++ ICE07/app/src/main/java/ca/georgiancollege/ice07/TVShowListAdapter.kt
@@ -0,0 +1,7 @@
+package ca.georgiancollege.ice07
+
+import androidx.recyclerview.widget.ListAdapter
+
+class TVShowListAdapter(private val onItemClicked: (TVShow) -> Unit): ListAdapter<TVShow, TVShowViewHolder>(TVShowComparator))
+{
+}
